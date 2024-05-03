db5af0c20549a3112105f2352aecf5d2d4c6e3e0
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-05-03 17:11:13 -0400

    fixed loop for carousel

--- src/App.js
+++ src/App.js
@@ -10,30 +10,52 @@ function App() {
   let [currentImage, setCurrentImage] = useState(photos[count]);
   //------------------------------------- [State]
 
+  // const changePic = () => {
+  //   setCount((count += 1));
+  //   console.log("Current Count: ", count);
+  //   setCurrentImage(photos[count]);
+  //   console.log("Current Image: ", currentImage);
+  // };
+  // // Cycle forward thru array
+  // const reversePic = () => {
+  //   setCount((count -= 1));
+  //   console.log("Current Count: ", count);
+  //   setCurrentImage(photos[count]);
+  //   console.log("Current Image: ", currentImage);
+  // };
+  //Cycle back thru pics
+  const changePic = () => {
+    setCount((prevCount) => {
+      const newCount = prevCount + 1;
+      if (newCount >= photos.length) {
+        // Reset count to 0 if it goes beyond the array length
+        return 0;
+      }
+      return newCount;
+    });
+    setCurrentImage(photos[count]);
+  };
 
-const changePic = () => {
-  setCount(count +=1);
-  console.log("Current Count: ", count);
-  setCurrentImage(photos[count]);
-  console.log("Current Image: ", currentImage);
-}
-// Cycle forward thru array
-const reversePic = () => {
-  setCount(count -=1);
-  console.log("Current Count: ", count);
-  setCurrentImage(photos[count]);
-  console.log("Current Image: ", currentImage);
-}
-//Cycle back thru pics
+  const reversePic = () => {
+    setCount((prevCount) => {
+      const newCount = prevCount - 1;
+      if (newCount < 0) {
+        // Reset count to the last index if it goes below 0
+        return photos.length - 1;
+      }
+      return newCount;
+    });
+    setCurrentImage(photos[count]);
+  };
 
   return (
     <div className="App">
       <h1> City Directory </h1>
-      <Card currentImg={currentImage}/>
+      <Card currentImg={currentImage} />
 
-            <div className="btnContainer">
-      <Button action={reversePic} icon={symbols.left}/>
-      <Button action={changePic} icon={symbols.right}/>
+      <div className="btnContainer">
+        <Button action={reversePic} icon={symbols.left} />
+        <Button action={changePic} icon={symbols.right} />
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card({currentImg}) {
+function Card({currentImg="https://images.unsplash.com/photo-1543791842-f91bcd359625?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHB1ZXJ0byUyMHJpY298ZW58MHx8MHx8fDA%3D"}) {
     return (
       <div className="cardContainer">
         <img  className="aPic" src={currentImg} alt="" />
